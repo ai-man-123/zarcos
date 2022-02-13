6c73c77fe98ca7ba08825e2159b5aa5d2d6fe0df
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-02-13 23:30:49 +0800

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+        "clientID": "7cSV6kRm8gIH0TS7JOBQ7w==",
+        "serverToken": "1@qBXFHlP6cTxGH3fcDk9a7UBPymKZgygpoEYlRx+fIxLd8eCuR2jcyuJjQ0c/AZk+Vy2OyLfAGXtq3g==",
+        "clientToken": "be2BAO9IvVGxxAWbeP+PlC89+jvHQmm2IcVKzB1WGTI=",
+        "encKey": "QhkpX0AWvtyDtaOmW+DSxnEFRyAe2Sc70ldrFIbgUJo=",
+        "macKey": "rw34LzAKNDZuY/Ad9oU1Zz85jfrFV04hFez8vtv3eWI="
+}
